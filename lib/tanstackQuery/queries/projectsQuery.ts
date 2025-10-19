0de0c38e8747f42844c245323d05cd2f53ff9b53
Obafemi Oludahunsi <oludahunsiobafemi18@gmail.com>
2025-10-19 19:13:42 +0300

    fix issue related to failed `preview`  deployment on vercel

--- lib/tanstackQuery/queries/projectsQuery.ts
+++ lib/tanstackQuery/queries/projectsQuery.ts
@@ -2,9 +2,11 @@ import { queryOptions } from "@tanstack/react-query";
 
 let baseUrl;
 
-process.env.NODE_ENV === "production"
+process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test"
   ? (baseUrl = process.env.BASE_URL)
   : (baseUrl = "http://localhost:3000");
+
+// const baseUrl = process.env.BASE_URL || "http://localhost:3000";
 
 interface ProjectTag {
   tag: string;
