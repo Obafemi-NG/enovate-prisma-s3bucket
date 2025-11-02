823f7c6b7b93cb38f5febe5c6e71977455475f8b
Obafemi Oludahunsi <oludahunsiobafemi18@gmail.com>
2025-11-02 21:07:28 +0300

    fix problem with `.env-related` issues

--- lib/tanstackQuery/queries/projectsQuery.ts
+++ lib/tanstackQuery/queries/projectsQuery.ts
@@ -1,7 +1,15 @@
 import { QueryClient, queryOptions, useQuery } from "@tanstack/react-query";
 import { getQueryClient } from "../get-query-client";
 
-const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+// const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+
+let baseUrl: string | undefined;
+
+if (process.env.NODE_ENV === "development") {
+  baseUrl = "http://localhost:3000";
+} else {
+  baseUrl = process.env.BASE_URL;
+}
 
 interface ProjectTag {
   tag: string;
@@ -17,6 +25,7 @@ export interface Project {
 }
 
 export const fetchProjects = async (): Promise<Project[]> => {
+  console.log(process.env.BASE_URL);
   const response = await fetch(`${baseUrl}/api/project`);
   if (!response.ok) throw new Error("Currently unable to fetch projects");
   return response.json();
